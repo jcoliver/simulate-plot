279920f7bdacc00099971dbf2c3970d078de4913
Jeffrey Oliver <jcoliver@arizona.edu>
2021-03-23 10:07:36 -0700

    Initial commit of data simulation and plot

---
+++ simulate-plot-data.R
@@ -0,0 +1,11 @@
+# Simulate and plot data
+# Jeff Oliver
+# jcoliver@arizona.edu
+# 2021-03-23
+
+# Simulate data from normal distribution
+x <- rnorm(n = 100)
+y <- 2 * x + rnorm(n = 100, sd = 0.1)
+
+# Plot simulated data
+plot(x = x, y = y)
